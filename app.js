4f6425b8a08210d97551aa6f0b3e574a33f232aa
Abraham Estrada <abraham.estrada@gmail.com>
2018-08-29 21:46:39 -0600

    Disable twitter if API keys not found

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-
-const instagram = require("./routes/instagram");
-const twitter = require("./routes/twitter");
+const config = require("./config");
 
 const { PORT = 3000 } = process.env;
 
@@ -10,8 +8,19 @@ app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
 
-app.use("/instagram", instagram);
-app.use("/twitter", twitter);
+app.use("/instagram", require("./routes/instagram"));
+
+if (
+  config.twitter &&
+  config.twitter.consumer_key &&
+  config.twitter.consumer_secret &&
+  config.twitter.access_token &&
+  config.twitter.access_token_secret
+) {
+  app.use("/twitter", require("./routes/twitter"));
+} else {
+  console.log("Twitter RSS is disabled for lacking config.");
+}
 
 console.log("listening on %s", PORT);
 app.listen(PORT);
